2597ad335c172b67dc6f33c7603c8006a1f3b2d2
prealge <34986064+prealge@users.noreply.github.com>
2018-06-06 16:11:35 -0400

    Update stories.js

--- stories.js
+++ stories.js
@@ -9,5 +9,5 @@ var stories = [
 
 
 for(var i = 0; i < stories.length; i++) {
-  document.getElementById('story-cards').innerHTML += '<div class="row"><div class="col s12 m6"><div class="card red darken-1"><div class="ca0rd-content white-text"><span class="card-title">' + stories[i].title + '</span><p>By ' + stories[i].author + '</p></div><div class="card-action"><a href="/' + stories[i].author.split(" ")[1] + '/' + stories[i].title + '">Read</a></div></div></div></div>';
+  document.getElementById('story-cards').innerHTML += '<div class="card" onclick="' + stories[i].author.split(" ")[1] + '/' + stories[i].title + '"><span class="card-title">' + stories[i].title + '</span><p>By ' + stories[i].author + '</p></div>';
 }

c2394afefda8d30cdcc74b6e678cad5f56f194cc
prealge <34986064+prealge@users.noreply.github.com>
2018-06-07 18:31:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var pageOn = "main";
 document.getElementById("main-page").style.transform = "translateX(0)";
 
 function openPage(page) {
-  document.getElementById(page + "-page").style.transform = "";
+  document.getElementById(page + "-page").style.transform = "translateX(0)";
   document.getElementById(pageOn + "-page").style.transform = "translateX(-100%)";
   pageOn = page;
 }

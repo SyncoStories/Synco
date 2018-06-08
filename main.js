84de6b44a555d6f04c8808eaf5a2d18198437c2e
prealge <34986064+prealge@users.noreply.github.com>
2018-06-08 08:05:56 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function hideAllPages() {
+  for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
+    document.getElementsByClassName("page")[i].style.display = "none";
+  }
+}
+
+if (window.location.href.split("?")[1] == "") {
+  hideAllPages();
+  document.getElementById("main-page").style.display = "block";
+} else {
+  document.getElementById(window.location.href.split("?")[1] + "-page").style.display = "block";
+}

2dba501ed97b226eae36a8ba74eeeef5f9407f1e
prealge <34986064+prealge@users.noreply.github.com>
2018-06-08 06:57:36 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var pageOn = "main";
-document.getElementById("main-page").style.display = "block";
-
-function openPage(page) {
-  document.getElementById(page + "-page").style.display = "block";
-  document.getElementById(pageOn + "-page").style.display = "none";
-  pageOn = page;
-}

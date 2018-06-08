ee2be7b4b134eab8776cf741884e8edf074c1f5d
prealge <34986064+prealge@users.noreply.github.com>
2018-06-08 14:36:49 -0400

    Update db.js

--- db.js
+++ db.js
@@ -18,7 +18,7 @@ firebase.database().ref().on("value", function(snapshot) {
 });
     
 for(var i = 0; i < Object.keys(stories).length; i++) {
-  document.getElementById('story-cards').innerHTML += '<div class="card" onclick="window.location.href = \'' + stories[i].author.split(" ")[1] + '/' + stories[i].title + '\'"><span class="card-title">' + stories[i].title + '</span><p>By ' + stories[i].author + '</p></div>';
+  document.getElementById('story-cards').innerHTML += '<div class="card" onclick="window.location.href = \'' + stories[Object.keys(stories)[i]].author + '/' + stories[Object.keys(stories)[i]].title + '\'"><span class="card-title">' + stories[i].title + '</span><p>By ' + stories[i].author + '</p></div>';
 }
 
 var user = firebase.auth().currentUser;

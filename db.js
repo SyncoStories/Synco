a01e1177c01dfd66492f93586d676a3f69a1c7a9
prealge <34986064+prealge@users.noreply.github.com>
2018-06-12 11:10:42 -0400

    Update db.js

--- db.js
+++ db.js
@@ -57,30 +57,6 @@ firebase.database().ref().once("value", function(snapshot) {
     document.getElementById('story-cards').innerHTML += '<div class="card" onclick="window.location.href = \'index.html?' + storySnapshot.key + '\'"><span class="card-title">' + storySnapshot.val().title + '</span><p>By ' + storySnapshot.val().author + '</p></div>';
   });
 });
-  
-//hides all pages
-function hideAllPages() {
-  for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
-    document.getElementsByClassName("page")[i].style.display = "none";
-  }
-}
-
-//detects page on
-if (!window.location.href.split("?")[1]) {
-  hideAllPages();
-  document.getElementById("main-page").style.display = "block";
-} else {
-  if(document.getElementById(window.location.href.split("?")[1] + "-page")) {
-    hideAllPages();
-    document.getElementById(window.location.href.split("?")[1] + "-page").style.display = "block";
-  } else {
-    hideAllPages();
-    document.getElementById("story-page").style.display = "block";
-    firebase.database().ref(window.location.href.split("?")[1]).on(function(snapshot) {
-      document.getElementById("story-page").innerHTML = snapshot.val().content;
-    });
-  }
-}
 
 setInterval(function() {
   if(firebase.auth().currentUser == firebase.auth().currentUser) {

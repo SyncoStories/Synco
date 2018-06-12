d0fbf38d51fabac57ea9f04dea80af6da60d8837
prealge <34986064+prealge@users.noreply.github.com>
2018-06-12 13:06:24 -0400

    Update create.js

--- create.js
+++ create.js
@@ -1,1 +1,7 @@
-
+function saveNewFile() {
+  firebase.database().ref().push({
+    title: document.getElememtById("story-title-input").value,
+    author: localStorage.name,
+    content: document.getElementById("story-text-area").innerHTML
+  });
+}

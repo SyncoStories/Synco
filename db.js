0165df19b549b20d71f9e6b14c0afa0fd887ac66
prealge <34986064+prealge@users.noreply.github.com>
2018-06-12 11:09:55 -0400

    Update db.js

--- db.js
+++ db.js
@@ -58,6 +58,29 @@ firebase.database().ref().once("value", function(snapshot) {
   });
 });
   
+//hides all pages
+function hideAllPages() {
+  for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
+    document.getElementsByClassName("page")[i].style.display = "none";
+  }
+}
+
+//detects page on
+if (!window.location.href.split("?")[1]) {
+  hideAllPages();
+  document.getElementById("main-page").style.display = "block";
+} else {
+  if(document.getElementById(window.location.href.split("?")[1] + "-page")) {
+    hideAllPages();
+    document.getElementById(window.location.href.split("?")[1] + "-page").style.display = "block";
+  } else {
+    hideAllPages();
+    document.getElementById("story-page").style.display = "block";
+    firebase.database().ref(window.location.href.split("?")[1]).on(function(snapshot) {
+      document.getElementById("story-page").innerHTML = snapshot.val().content;
+    });
+  }
+}
 
 setInterval(function() {
   if(firebase.auth().currentUser == firebase.auth().currentUser) {

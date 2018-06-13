23f9d73bf20f9c01e3a7433e7de481420eb9637d
SirAricost <coffee-media@users.noreply.github.com>
2018-06-13 09:53:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+function createNewStory() {
+  var newStory = firebase.database().ref().push({
+    title: document.getElementById("story-title-input").value,
+    author: localStorage.name,
+    content: document.getElementById("story-text-area").value
+  });
+  window.location.href = "index.html?" + newStory.key;
+}
+
+function saveStory() { 
+  if(window.location.href.split("?")[1]) {
+    firebase.database().ref(window.location.href.split("?")[1]).set({
+      title: document.getElementById("story-title-input").value,
+      author: localStorage.name,
+      content: document.getElementById("story-text-area").value;
+    });
+  }
+}
+
+
+
 function hideAllPages() {
   for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
     document.getElementsByClassName("page")[i].style.display = "none";

2acf1b8ac849184a69da35ed9424c42b574fa17a
SirAricost <coffee-media@users.noreply.github.com>
2018-06-13 09:57:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createNewStory() {
 
 function saveStory() { 
   if(window.location.href.split("?")[1]) {
-    firebase.database().ref(window.location.href.split("?")[1]).set({
+    firebase.database().ref().child(window.location.href.split("?")[1]).set({
       title: document.getElementById("story-title-input").value,
       author: localStorage.name,
       content: document.getElementById("story-text-area").value

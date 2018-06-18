e845694dccfbf4e93f605ae2bc769b3428d6e57a
prealge <34986064+prealge@users.noreply.github.com>
2018-06-18 13:16:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function createNewStory() {
   var newStory = firebase.database().ref().child("stories").push({
     title: document.getElementById("story-title-input").value,
     author: localStorage.name,
-    content: document.getElementById("story-text-area").innerText,
+    content: document.getElementById("story-text-area").innerHTML,
     likes: 0
   });
   window.location.href = "index.html?" + newStory.key;

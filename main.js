ef219a994c4ca74a36b6ac9dca85dcb98fa6e220
prealge <34986064+prealge@users.noreply.github.com>
2018-06-25 19:45:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ function createNewStory() {
     content: document.getElementById("story-text-area").innerHTML,
     likes: 0
   });
-  window.location.href = "index.html?" + newStory.key;
+  firebase.database().ref().on("child_added", function() {
+    window.location.href = "?" + newStory.key
+  });
 }
 
 function saveStory() { 

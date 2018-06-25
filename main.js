1acb0f72d3138d314962815392673e7452f628f7
prealge <34986064+prealge@users.noreply.github.com>
2018-06-25 19:37:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function createNewStory() {
     content: document.getElementById("story-text-area").innerHTML,
     likes: 0
   });
-  window.location.href = "?" + newStory.key;
+  window.location.href = "index.html?" + newStory.key;
 }
 
 function saveStory() { 

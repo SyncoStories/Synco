7b8b83cf93ae9bf46c30738630d2b96ff1566533
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-07-07 16:00:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const admins = ['prealge', 'Tel-of-lolth', 'synco', 'isy123'];
+const admins = ['prealge', 'tel-of-lolth', 'synco', 'isy123'];
 
 function createNewStory() {
   var newStory = firebase.database().ref().child("stories").push({

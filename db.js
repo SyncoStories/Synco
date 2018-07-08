6356cf18a49ce87d745205ae1080567581bf90c4
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-07-08 09:08:15 -0400

    Update db.js

--- db.js
+++ db.js
@@ -46,7 +46,7 @@ function signout() {
 
 firebase.auth().onAuthStateChanged(function(user) {
   if(user) {
-    document.getElementById("navbar-username").innerHTML = "<a href='?profile'>" + user.email.replace("@fakeemail.com", "") + "</a> <button class='btn-primary' style='background-color: red; color: white; font-size: 100%;'><a href='?edit' style='color: white;'>Make a New Story</a></button> <button class='btn-primary' style='background-color: red; margin: 0; font-size: 100%;' onclick='signout()'><a> Logout</a> </button>";
+    document.getElementById("navbar-username").innerHTML = "<a>" + user.email.replace("@fakeemail.com", "") + "</a> <button class='btn-primary' style='background-color: red; color: white; font-size: 100%;'><a href='?edit' style='color: white;'>Make a New Story</a></button> <button class='btn-primary' style='background-color: red; margin: 0; font-size: 100%;' onclick='signout()'><a> Logout</a> </button>";
   } else {
     document.getElementById("navbar-username").innerHTML = "<button class='btn-primary' style='background-color: red; margin: 0; font-size: 100%;'><a href=\"index.html?login\">Login</a></button> or <button class='btn-primary' style='background-color: red; margin: 0; font-size: 100%;'><a href=\"index.html?signup\">Signup</a></button>";
   }

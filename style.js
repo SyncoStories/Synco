df45c9eccb54ca04611c3df0d12a5f6e7012bb15
prealge <34986064+prealge@users.noreply.github.com>
2018-07-31 09:23:18 -0400

    Create style.js

---
+++ style.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < document.getElementsByClassName("dropdown").length; i++) {
+  document.getElementsByClassName("dropdown")[i].onclick = function() {
+    this.getElementsByTagName("ul")[0].style.maxHeight = '100px';
+  }
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.toggle')) {
+    for (var i = 0; i < document.getElementsByClassName("dropdown").length; i++) {
+      document.getElementsByClassName("dropdown")[i].getElementsByTagName("ul")[0].style.maxHeight = '0';
+    }
+  }
+}

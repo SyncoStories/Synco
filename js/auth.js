829603a5ce24b3f2ea9ebd0201b0103862a0332a
prealge <34986064+prealge@users.noreply.github.com>
2018-09-10 15:53:14 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
 var user = firebase.auth().currentUser;
+
+function login() {
+  firebase.auth().signInWithEmailAndPassword(document.getElementById('signin-uname').innerHTML + '@fakeemail.com', document.getElementById('signin-password')).then(function() {
+    console.log('Successfuly logged in!');
+    window.location.href = '/';
+  }).catch(function(error) {
+    throw error;
+    alert(error);
+  });
+}

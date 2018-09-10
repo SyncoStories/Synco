1599ad7533fb4f3a650cef1f00b3ba86b548ae01
prealge <34986064+prealge@users.noreply.github.com>
2018-09-10 15:58:01 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,19 @@
 var user = firebase.auth().currentUser;
 
 function login() {
-  firebase.auth().signInWithEmailAndPassword(document.getElementById('signin-uname').innerHTML + '@fakeemail.com', document.getElementById('signin-password')).then(function() {
+  firebase.auth().signInWithEmailAndPassword(document.getElementById('signin-uname').value + '@fakeemail.com', document.getElementById('signin-password').value).then(function() {
     console.log('Successfuly logged in!');
+    window.location.href = '/';
+  }).catch(function(error) {
+    throw error;
+    alert(error);
+  });
+}
+
+
+function signup() {
+  firebase.auth().creteUserWithEmailAndPassword(document.getElementById('new-account-uname').value + '@fakeemail.com', document.getElementById('new-user-password').value).then(function() {
+    console.log('Your account has been created!');
     window.location.href = '/';
   }).catch(function(error) {
     throw error;

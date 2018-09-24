134a882c27725f3c0570b26ce2c24eb4b2f4961b
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-09-24 10:22:43 -0400

    Update main.js

--- Synco-8ce1680897b9278fddabc573e4ebd1f9c24bf8d2/main.js
+++ Synco-8ce1680897b9278fddabc573e4ebd1f9c24bf8d2/main.js
@@ -157,7 +157,7 @@ if (!window.location.href.split("?")[1]) {
             document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='material-icons'>thumb_up_alt</i>t</button>";
           }
           if(localStorage.name == snapshot.val().author) {
-            document.getElementById("story-page").innerHTML += "<button class='btn-primary round-hover' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><div class='pencil-icon'><i></i></div></button>";
+            document.getElementById("story-page").innerHTML += "<button class='btn-primary round-hover' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><div class='material-icons'>edit</div></button>";
           }
       } else {
         document.getElementById("story-404-page").style.display = "block";

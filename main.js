a860689bc2003d96cc322783f26d944a1a55bda0
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-09-26 17:35:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -164,7 +164,6 @@ if (!window.location.href.split("?")[1]) {
             document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i>";
           }
           if(localStorage.name == snapshot.val().author) {
-            download(snapshot.val().title + ".txt", snapshot.val().content);
             document.getElementById("story-page").innerHTML += "<button class='btn-primary' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><i class='fas fa-edit'></i></button> <a class='btn-primary' id='Download' style='right: 5px;'><i class='fas fa-download'></i></a>";
           }
       } else {
@@ -172,5 +171,6 @@ if (!window.location.href.split("?")[1]) {
       }
     });
   }
+  download(snapshot.val().title + ".txt", snapshot.val().content);
 }
 

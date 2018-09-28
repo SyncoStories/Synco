9312f28a4fcbdfd23d8dd7b6162244e137a05f3e
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-09-28 11:15:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -158,7 +158,7 @@ if (!window.location.href.split("?")[1]) {
             }
           }
           if (localStorage.name !== "null") {
-            document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i><a class='btn-primary' id='Download' style='right: 5px;' download='" + ValTitle + ".html' href='data:text/plain;charset=utf-8," + ValContent + "'><i class='fas fa-upload'></i></a>";
+            document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i> </button> <a class='btn-primary' id='Download' style='right: 5px;' download='" + ValTitle + ".html' href='data:text/plain;charset=utf-8," + ValContent + "'><i class='fas fa-upload'></i></a>";
 
           }
           if(localStorage.name == snapshot.val().author) {

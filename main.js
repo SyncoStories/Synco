e45ed921d0397fa1217624a58d1f4da7a28e9e68
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2018-10-05 15:04:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createNewStory() {
     }
   });
   window.location.href = "?" + newStory.key;
-  document.getElementById("editBtn").click;
+  document.getElementById("editBtn").click();
 }
 
 function saveStory() {

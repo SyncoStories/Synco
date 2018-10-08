648aee3060364d844e6d2bd131122ed3223a89d7
prealge <34986064+prealge@users.noreply.github.com>
2018-10-08 11:00:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ if (!window.location.href.split("?")[1]) {
   db.collection("stories").get().then(function(snapshot) {
     document.getElementById('story-cards').innerHTML = '';
     snapshot.forEach(function(storySnapshot) {
-      document.getElementById('story-cards').innerHTML += '<span class="card" onclick="window.location.href = \'https://synco.tk/?' + storySnapshot.key + '\'"><font class="card-title">' + storySnapshot.data().title + '</font><p><i class="fas fa-address-card"></i> ' + storySnapshot.data().author + ' </p><p><i class="fas fa-thumbs-up"></i> ' + storySnapshot.data().public.likes * -1 + '</p></span>';
+      document.getElementById('story-cards').innerHTML += '<span class="card" onclick="window.location.href = \'https://synco.tk/?' + storySnapshot.id + '\'"><font class="card-title">' + storySnapshot.data().title + '</font><p><i class="fas fa-address-card"></i> ' + storySnapshot.data().author + ' </p><p><i class="fas fa-thumbs-up"></i> ' + storySnapshot.data().public.likes * -1 + '</p></span>';
     });
   });
 } else {

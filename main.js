b5df677c4585961dec1e2c6edcfdd0f6a18a194d
prealge <34986064+prealge@users.noreply.github.com>
2018-10-08 11:41:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function editStory(storyId) {
 function createNewStory() {
   db.collection("stories").add({
     title: "Untitled Story",
-    author: displayName,
+    author: localStorage.name,
     uid: firebase.auth().currentUser.uid,
     content: "",
     tags: [],
@@ -61,7 +61,8 @@ function saveStory() {
   if (window.location.href.split("?")[1]) {
     db.collection("stories").doc(window.location.href.split("?")[1]).update({
       title: document.getElementById("story-title-input").value,
-      author: displayName,
+      author: localStorage.name
+      ,
       content: document.getElementById("story-text-area").innerHTML,
       tags: document.getElementById("tags").innerHTML.split(",")
     });
@@ -144,11 +145,11 @@ if (!window.location.href.split("?")[1]) {
               document.getElementById("story-page").innerHTML += "<tag>" + snapshot.data().tags[i] + "</tag>";
             }
           }
-          if (displayName !== "null" && snapshot.data().title !== "Synco: A Work in Progress") {
+          if (localStorage.name !== "null" && snapshot.data().title !== "Synco: A Work in Progress") {
             document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i> </button> <a class='btn-primary' id='Download' style='right: 5px;' download='" + ValTitle + ".html' href='data:text/plain;charset=utf-8," + ValContent + "'> <i class='fas fa-upload'></i></a>";
 
           }
-          if(displayName == snapshot.data().author) {
+          if(localStorage.name == snapshot.data().author) {
             document.getElementById("story-page").innerHTML += "<button class='btn-primary' id='editBtn' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><i class='fas fa-edit'></i></button>";          
           }
       } else {

25fd898f4f8a0c4dce44b65519e63a33d3df2133
prealge <34986064+prealge@users.noreply.github.com>
2018-10-16 15:10:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function createNewStory() {
     content: document.getElementById("story-text-area").innerHTML,
     uid: firebase.auth().currentUser.uid,
     tags: document.getElementById("tags-input").value.split(","),
-    likes: 0;
+    likes: 0
   }).then(function(docRef) {
     window.location.href = "?" + docRef.id;
   }).catch(function(error) {

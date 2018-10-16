93bd0d20ca7faeec6b0ea58a28274874c427d8de
prealge <34986064+prealge@users.noreply.github.com>
2018-10-16 15:05:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ var options = {
     "data.title",
     "data.author",
     "data.tags"
-]
+  ]
 };
 
 function editStory(storyId) {
   hideAllPages();
   db.collection("stories").doc(storyId).get().then(function(snapshot) {
     if (snapshot.data().author == localStorage.name) {
-          document.getElementById("edit-page").style.display = "block";
-          document.getElementById("story-title-input").value = snapshot.data().title;
-          document.getElementById("story-text-area").innerHTML = snapshot.data().content;
-          document.getElementById("tags-input").value = snapshot.data().tags;
-          document.getElementById("save-story-btn").onclick = saveStory;
+      document.getElementById("edit-page").style.display = "block";
+      document.getElementById("story-title-input").value = snapshot.data().title;
+      document.getElementById("story-text-area").innerHTML = snapshot.data().content;
+      document.getElementById("tags-input").value = snapshot.data().tags;
+      document.getElementById("save-story-btn").onclick = saveStory;
     }
   });
   document.getElementById("story-text-area").onkeydown = function(e) {
-    if(e.keyCode == 9) {
+    if (e.keyCode == 9) {
       document.execCommand("insertHTML", false, " ");
       return false;
     }
@@ -36,16 +36,17 @@ function editStory(storyId) {
 
 function createNewStory() {
   db.collection("stories").add({
-      title: document.getElementById("story-title-input").value,
-      author: localStorage.name,
-      content: document.getElementById("story-text-area").innerHTML,
-      tags: document.getElementById("tags-input").value.split(","),
-      public: {
-        favorites: 0,
-        likes: 0,
-        views: 0,
-      }
-    }).then(function(docRef) {
+    title: document.getElementById("story-title-input").value,
+    author: localStorage.name,
+    content: document.getElementById("story-text-area").innerHTML,
+    uid: firebase.auth().currentUser.uid,
+    tags: document.getElementById("tags-input").value.split(","),
+    public: {
+      favorites: 0,
+      likes: 0,
+      views: 0,
+    }
+  }).then(function(docRef) {
     window.location.href = "?" + docRef.id;
   }).catch(function(error) {
     alert(error);
@@ -79,7 +80,7 @@ function getStoryInfo(storyId) {
 }
 
 function deleteStory(storyId) {
-  if(confirm("Are you sure that you would like to delete this story? All data will be permanently lost.")) {
+  if (confirm("Are you sure that you would like to delete this story? All data will be permanently lost.")) {
     db.collection("stories").doc(storyId).delete().then(function() {
       alert("The story has been successfully deleted!");
     }).catch(function(error) {
@@ -98,14 +99,19 @@ function searchStories(search) {
   hideAllPages();
   document.getElementById('main-page').style.display = 'block';
   document.getElementById('story-cards').innerHTML = '<br><br><h4 style="color: lightgray">Loading . . .</h4>';
-  firebase.database().ref("stories").once("value",function(snapshot) {
-    var stories = new Fuse(Object.keys(snapshot.data()).map(function(key) {return {data: snapshot.data()[key], key: key}}), options).search(search);
-    if(stories.length == 0) {
+  firebase.database().ref("stories").once("value", function(snapshot) {
+    var stories = new Fuse(Object.keys(snapshot.data()).map(function(key) {
+      return {
+        data: snapshot.data()[key],
+        key: key
+      }
+    }), options).search(search);
+    if (stories.length == 0) {
       document.getElementById('story-cards').innerHTML = '<br><br><h4 style="color: lightgray">No Stories Matched Your Search</h4>';
     } else {
       document.getElementById('story-cards').innerHTML = '';
     }
-    for(var i = 0; i < stories.length; i++) {
+    for (var i = 0; i < stories.length; i++) {
       if (stories[i].data.public.likes == -1) {
         var likeLikes = "Like"
       } else {
@@ -132,27 +138,26 @@ if (!window.location.href.split("?")[1]) {
   } else {
     db.collection("stories").doc(window.location.href.split("?")[1]).get().then(function(snapshot) {
       if (snapshot.data()) {
-          var ValContent = snapshot.data().content;
-          var ValTitle = snapshot.data().title;
-          document.getElementById("story-page").style.display = "block";
-          document.getElementById("story-page").innerHTML = "<center><h1>" + snapshot.data().title + "</h1><h5> By " + snapshot.data().author + "</h5></center><div>" + snapshot.data().content + "</div>";
-          if (snapshot.data().tags) {
-            for (var i = 0; i < snapshot.data().tags.length; i++) {
-              document.getElementById("story-page").innerHTML += "<tag>" + snapshot.data().tags[i] + "</tag>";
-            }
+        var ValContent = snapshot.data().content;
+        var ValTitle = snapshot.data().title;
+        document.getElementById("story-page").style.display = "block";
+        document.getElementById("story-page").innerHTML = "<center><h1>" + snapshot.data().title + "</h1><h5> By " + snapshot.data().author + "</h5></center><div>" + snapshot.data().content + "</div>";
+        if (snapshot.data().tags) {
+          for (var i = 0; i < snapshot.data().tags.length; i++) {
+            document.getElementById("story-page").innerHTML += "<tag>" + snapshot.data().tags[i] + "</tag>";
           }
-          if (localStorage.name !== "null" && snapshot.data().title !== "Synco: A Work in Progress") {
-            document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i> </button> <a class='btn-primary' id='Download' style='right: 5px;' download='" + ValTitle + ".html' href='data:text/plain;charset=utf-8," + ValContent + "'> <i class='fas fa-upload'></i></a>";
+        }
+        if (localStorage.name !== "null" && snapshot.data().title !== "Synco: A Work in Progress") {
+          document.getElementById("story-page").innerHTML += "<br><br><button class='btn-primary' onclick='likeStory(\"" + window.location.href.split("?")[1] + "\")'><i class='fas fa-thumbs-up'></i> </button> <a class='btn-primary' id='Download' style='right: 5px;' download='" + ValTitle + ".html' href='data:text/plain;charset=utf-8," + ValContent + "'> <i class='fas fa-upload'></i></a>";
 
-          }
-          if(localStorage.name == snapshot.data().author) {
-            document.getElementById("story-page").innerHTML += "<button class='btn-primary' id='editBtn' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><i class='fas fa-edit'></i></button>";          
-          }
+        }
+        if (localStorage.name == snapshot.data().author) {
+          document.getElementById("story-page").innerHTML += "<button class='btn-primary' id='editBtn' onclick='editStory(\"" + window.location.href.split("?")[1] + "\")' style='right: 5px;'><i class='fas fa-edit'></i></button>";
+        }
       } else {
         document.getElementById("story-404-page").style.display = "block";
       }
     });
   }
- 
-}
 
+}

a24319fec8656ad587a5a3a0a2c1dd5a99855f49
prealge <34986064+prealge@users.noreply.github.com>
2018-10-18 11:09:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -97,6 +97,17 @@ function deleteStory(storyId) {
   }
 }
 
+function loadStories(ref) {
+  hideAllPages();
+  document.getElementById("main-page").style.display = "block";
+  ref.get().then(function(snapshot) {
+    document.getElementById('story-cards').innerHTML = '';
+    snapshot.forEach(function(storySnapshot) {
+      document.getElementById('story-cards').innerHTML += '<span class="card" onclick="window.location.href = \'https://synco.tk/?' + storySnapshot.id + '\'"><font class="card-title">' + storySnapshot.data().title + '</font><p><i class="fas fa-address-card"></i> ' + storySnapshot.data().author + ' </p><p><i class="fas fa-thumbs-up"></i> ' + storySnapshot.data().likes + '</p></span>';
+    });
+  });
+}
+
 function hideAllPages() {
   for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
     document.getElementsByClassName("page")[i].style.display = "none";
@@ -106,21 +117,10 @@ function hideAllPages() {
 
 hideAllPages();
 if (!window.location.href.split("?")[1]) {
-  document.getElementById("main-page").style.display = "block";
-  db.collection("stories").orderBy("likes", "desc").where("publised", "==", true).get().then(function(snapshot) {
-    document.getElementById('story-cards').innerHTML = '';
-    snapshot.forEach(function(storySnapshot) {
-      document.getElementById('story-cards').innerHTML += '<span class="card" onclick="window.location.href = \'https://synco.tk/?' + storySnapshot.id + '\'"><font class="card-title">' + storySnapshot.data().title + '</font><p><i class="fas fa-address-card"></i> ' + storySnapshot.data().author + ' </p><p><i class="fas fa-thumbs-up"></i> ' + storySnapshot.data().likes + '</p></span>';
-    });
-  });
+  loadStories(db.collection("stories").orderBy("likes", "desc").where("publised", "==", true));
 } else {
   if (window.location.href.split("?")[1] == "mystories") {
-    db.collection("stories").orderBy("likes", "desc").where("uid", "==", firebase.auth().currentUser.uid).get().then(function(snapshot) {
-      document.getElementById('story-cards').innerHTML = '';
-      snapshot.forEach(function(storySnapshot) {
-        document.getElementById('story-cards').innerHTML += '<span class="card" onclick="window.location.href = \'https://synco.tk/?' + storySnapshot.id + '\'"><font class="card-title">' + storySnapshot.data().title + '</font><p><i class="fas fa-address-card"></i> ' + storySnapshot.data().author + ' </p><p><i class="fas fa-thumbs-up"></i> ' + storySnapshot.data().likes + '</p></span>';
-      });
-    });
+    loadStories(db.collection("stories").orderBy("likes", "desc").where("uid", "==", firebase.auth().currentUser.uid));
   } else if (document.getElementById(window.location.href.split("?")[1] + "-page")) {
     document.getElementById(window.location.href.split("?")[1] + "-page").style.display = "block"
   } else {
